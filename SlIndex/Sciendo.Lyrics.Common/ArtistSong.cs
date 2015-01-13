aabf2a4db463ab6b0b147958ff8e89f719e6b6d1
oduma <tavi_duma2002@yahoo.co.uk>
2015-01-13 22:31:43 +0000

    and somemore changes

---
+++ SlIndex/Sciendo.Lyrics.Common/ArtistSong.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Sciendo.Lyrics.Common
+{
+    public class ArtistSong
+    {
+        [XmlIgnore]
+        public string artist { get; set; }
+        [XmlIgnore]
+        public string song { get; set; }
+
+    }
+}
