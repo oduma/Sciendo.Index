556d3fbe51e89aaa4d44d93524c6f47f3b61218a
oduma <tavi_duma2002@yahoo.co.uk>
2015-02-16 12:47:59 +0000

    added the start of an indexing agent

---
+++ SlIndex/Sciendo.Indexer.Contracts/IIndexerAgent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sciendo.Indexer.Contracts
+{
+    public interface IIndexerAgent
+    {
+        void IndexLyricsOnDemand(string fromPath);
+        void IndexMusicOnDemand(string fromPath, bool includeLyrics=false );
+
+    }
+}
