60b64bfc52ff13dce1a7f8186a54f5fdcab65e2e
oduma <tavi_duma2002@yahoo.co.uk>
2015-05-22 16:29:20 +0100

    some old shite

--- SlIndex/Sciendo.Music.Contracts/Solr/Document.cs
+++ SlIndex/Sciendo.Music.Contracts/Solr/Document.cs
@@ -7,27 +7,13 @@ namespace Sciendo.Music.Contracts.Solr
     public class Document
     {
         private const string Prefix = "file:///";
-        public Document()
-        { }
 
-        public Document (string filePath, string catalogLetter)
-        {
-            if (string.IsNullOrEmpty(filePath))
-                throw new ArgumentNullException("filePath");
-            FilePathId = filePath.ToLower();
-            file_path = new Field<string> { Set = Prefix + filePath };
-
-            ExtensionF = new Field<string> { Set = Path.GetExtension(FilePathId).Replace(".", "") };
-            LetterCatalogF = new Field<string> { Set = catalogLetter };
-
-        }
-
-        public Document(string filePath, string catalogLetter, string songLyrics)
-            :this(filePath,catalogLetter)
-        {
-            Lyrics =  new Field<string> {Set = songLyrics};
-        }
-
+        [JsonProperty("artist")]
+        public Field<string[]> Artist { get; set; }
+        [JsonProperty("title")]
+        public Field<string> Title { get; set; }
+        [JsonProperty("album")]
+        public Field<string> Album { get; set; }
         [JsonProperty("file_path_id")]
         public string FilePathId { get; set; }
         [JsonProperty("file_path")]
@@ -38,5 +24,30 @@ namespace Sciendo.Music.Contracts.Solr
         public Field<string> LetterCatalogF { get; set; }
         [JsonProperty("lyrics")]
         public Field<string> Lyrics { get; set; }
+
+        public Document(string filePath, string catalogLetter, string[] artists, string song, string albumName)
+        {
+            if (string.IsNullOrEmpty(filePath))
+                throw new ArgumentNullException("filePath");
+            FilePathId = filePath.ToLower();
+            file_path = new Field<string> { Set = Prefix + filePath };
+
+            ExtensionF = new Field<string> { Set = Path.GetExtension(FilePathId).Replace(".", "") };
+            LetterCatalogF = new Field<string> { Set = catalogLetter };
+
+            Artist = new Field<string[]> { Set = artists };
+            Title = new Field<string> { Set = song };
+            Album = new Field<string> { Set = albumName };
+        }
+
+        public Document():base()
+        {
+            
+        }
+
+        public Document(string filePath, string catalogLetter, string[] artists, string songTitle, string albumName, string lyrics):this(filePath,catalogLetter,artists,songTitle,albumName)
+        {
+            Lyrics = new Field<string> {Set = lyrics};
+        }
     }
 }
